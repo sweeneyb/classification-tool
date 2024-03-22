191b72034d3251c41431e7ccc3afe4fe43c1c08b
Brian Sweeney <tab126@gmail.com>
2024-03-21 20:24:54 -0400

    pull in full data

--- src/preload.ts
+++ src/preload.ts
@@ -1,7 +1,7 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 const { contextBridge } = require('electron');
-
+const {allData} = require('./data.ts')
 
 var data = {"total":9556,"objectIDs":[844978,490012,485821,207021]}
 // Define your constants
@@ -11,6 +11,6 @@ const MY_CONSTANT = 'Hello from preload!';
 contextBridge.exposeInMainWorld(
   'myAPI', {
     MY_CONSTANT: MY_CONSTANT,
-    data: data
+    data: allData
   }
 );
