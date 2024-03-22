4879978b4e63c7b2fd77a562f2e41fffd7639be5
Brian Sweeney <tab126@gmail.com>
2024-03-21 22:11:03 -0400

    webpack config for electron

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -10,6 +10,7 @@ export const mainConfig: Configuration = {
    */
   entry: './src/index.ts',
   // Put your normal webpack config below here
+  target: 'electron-main',
   module: {
     rules,
   },
